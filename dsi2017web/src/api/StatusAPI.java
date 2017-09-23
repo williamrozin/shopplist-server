819528cf109f9eda3a51796bffa6aba7f1ba6562
William Rozin Gaspar <williamrozingaspar@gmail.com>
2017-09-22 21:29:16 -0300

    created basic classes

---
+++ dsi2017web/src/api/StatusAPI.java
@@ -0,0 +1,16 @@
+package api;
+
+import javax.enterprise.context.RequestScoped;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
+@Path("/status")
+@RequestScoped
+public class StatusAPI {
+	
+	@GET
+	public String getClientes(){
+		return "I am alive";
+	}
+	
+}
